1f49219999b253d9c40c6bfda73c612ab60a417f
Friendweb <90310837+Friendweb@users.noreply.github.com>
2024-03-28 10:10:12 +0300

    add comment for autotest

--- test_main.py
+++ test_main.py
@@ -3,11 +3,13 @@ import datetime
 import json
 import requests
 
+# Функция для создания папки для хранения логов
 def create_logs_folder():
     logs_folder = "logs"
     if not os.path.exists(logs_folder):
         os.makedirs(logs_folder)
 
+# Функция для создания папки для хранения логов за текущий месяц
 def create_monthly_logs_folder():
     current_month = datetime.datetime.now().strftime("%B").lower()
     logs_month_folder = os.path.join("logs", f"logs_{current_month}")
@@ -15,9 +17,11 @@ def create_monthly_logs_folder():
         os.makedirs(logs_month_folder)
     return logs_month_folder
 
+# Функция для получения временной метки
 def get_timestamp():
     return datetime.datetime.now().strftime("%Y-%m-%d_%H-%M")
 
+# Функция для логирования запроса
 def log_request(url, headers):
     logs_folder = create_monthly_logs_folder()
     with open(os.path.join(logs_folder, f"logs_{get_timestamp()}.txt"), "a") as file:
@@ -25,6 +29,7 @@ def log_request(url, headers):
         file.write(f"URL: {url}\n")
         file.write(f"Headers: {headers}\n\n")
 
+# Функция для логирования ответа
 def log_response(response):
     logs_folder = create_monthly_logs_folder()
     with open(os.path.join(logs_folder, f"logs_{get_timestamp()}.txt"), "a") as file:
@@ -36,18 +41,20 @@ def log_response(response):
         json.dump(response_data, file, indent=4)
         file.write("\n\n")
 
+# Тестовая функция для поиска информации о Spider-Man 2
 def test_search_spiderman2():
+    # URL для запроса к API
     url = "https://plati.io/api/search.ashx?query=spider-man-2&visibleOnly=true&response=json"
-    headers = {"User-Agent": "Mozilla/5.0"}
+    headers = {"User-Agent": "Mozilla/5.0"}  # Заголовки для запроса
 
-    create_logs_folder()
+    create_logs_folder()  # Создание папки для хранения логов
 
-    log_request(url, headers)
+    log_request(url, headers)  # Логирование запроса
 
     # Отправляем GET-запрос к API
     response = requests.get(url, headers=headers)
 
-    log_response(response)
+    log_response(response)  # Логирование ответа
 
     # Проверяем успешность ответа (статус код 200)
     assert response.status_code == 200
