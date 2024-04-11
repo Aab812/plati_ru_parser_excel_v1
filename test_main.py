c9380eca0cc3602efd23586d351ea13164379780
Friendweb <90310837+Friendweb@users.noreply.github.com>
2024-04-12 00:39:25 +0300

    add comment for autotest

--- test_main.py
+++ test_main.py
@@ -2,36 +2,33 @@ import os
 import datetime
 import json
 import requests
+import logging
 
-# Функция для создания папки для хранения логов
+# Создание папки для хранения логов
 def create_logs_folder():
     logs_folder = "logs"
-    if not os.path.exists(logs_folder):
-        os.makedirs(logs_folder)
+    os.makedirs(logs_folder, exist_ok=True)
 
-# Функция для создания папки для хранения логов за текущий месяц
+# Создание папки для хранения логов за текущий месяц
 def create_monthly_logs_folder():
     current_month = datetime.datetime.now().strftime("%B").lower()
     logs_month_folder = os.path.join("logs", f"logs_{current_month}")
-    if not os.path.exists(logs_month_folder):
-        os.makedirs(logs_month_folder)
+    os.makedirs(logs_month_folder, exist_ok=True)
     return logs_month_folder
 
-# Функция для получения временной метки
+# Получение временной метки
 def get_timestamp():
     return datetime.datetime.now().strftime("%Y-%m-%d_%H-%M")
 
-# Функция для логирования запроса
-def log_request(url, headers):
-    logs_folder = create_monthly_logs_folder()
+# Логирование запроса
+def log_request(url, headers, logs_folder):
     with open(os.path.join(logs_folder, f"logs_{get_timestamp()}.txt"), "a") as file:
         file.write("== Request ==\n")
         file.write(f"URL: {url}\n")
         file.write(f"Headers: {headers}\n\n")
 
-# Функция для логирования ответа
-def log_response(response):
-    logs_folder = create_monthly_logs_folder()
+# Логирование ответа
+def log_response(response, logs_folder):
     with open(os.path.join(logs_folder, f"logs_{get_timestamp()}.txt"), "a") as file:
         file.write("== Response ==\n")
         file.write(f"URL: {response.url}\n")
@@ -43,42 +40,39 @@ def log_response(response):
 
 # Тестовая функция для поиска информации о Spider-Man 2
 def test_search_spiderman2():
-    # URL для запроса к API
     url = "https://plati.io/api/search.ashx?query=spider-man-2&visibleOnly=true&response=json"
-    headers = {"User-Agent": "Mozilla/5.0"}  # Заголовки для запроса
+    headers = {"User-Agent": "Mozilla/5.0"}
 
     create_logs_folder()  # Создание папки для хранения логов
+    logs_folder = create_monthly_logs_folder()  # Создание папки для хранения логов за текущий месяц
 
-    log_request(url, headers)  # Логирование запроса
+    log_request(url, headers, logs_folder)  # Логирование запроса
 
-    # Отправляем GET-запрос к API
-    response = requests.get(url, headers=headers)
+    try:
+        response = requests.get(url, headers=headers)  # Отправка GET-запроса к API
+        response.raise_for_status()  # Проверка успешности ответа
+    except requests.exceptions.RequestException as e:
+        logging.error(f"Request to {url} failed: {e}")
+    else:
+        log_response(response, logs_folder)  # Логирование ответа
 
-    log_response(response)  # Логирование ответа
+        # Проверка, что ответ в формате JSON
+        content_type = response.headers['Content-Type']
+        if "application/json" in content_type or 'text/json' in content_type:
+            response_data = response.json()
+            keys = {key for item in response_data.get('items', []) for key in item}
+            used_keys_order = ["id", "name", "price_rur", "url", "description", "seller_id", "seller_name",
+                               "seller_rating", "count_positiveresponses", "count_negativeresponses", "count_returns"]
+            print("\nИспользуемые ключи:")
+            for key in used_keys_order:
+                if key in keys:
+                    print(key)
 
-    # Проверяем успешность ответа (статус код 200)
-    assert response.status_code == 200
+        # Выводим статус код в терминал
+        print("Статус код:", response.status_code)
 
-    # Проверяем, что ответ в формате JSON
-    assert "application/json" in response.headers['Content-Type'] or 'text/json' in response.headers['Content-Type']
-
-    # Выводим только уникальные ключи из ответа метода в заданном порядке
-    response_data = response.json()
-    keys = set()
-    for item in response_data['items']:
-        keys.update(item.keys())
-    used_keys_order = ["id", "name", "price_rur", "url", "description", "seller_id", "seller_name",
-                 "seller_rating", "count_positiveresponses", "count_negativeresponses", "count_returns"]
-    print("\nИспользуемые ключи:")
-    for key in used_keys_order:
-        if key in keys:
-            print(key)
-
-    # Выводим статус код в терминал
-    print("Статус код:", response.status_code)
-
-    # Выводим URL в терминал
-    print("URL:", url)
+        # Выводим URL в терминал
+        print("URL:", url)
 
 if __name__ == '__main__':
     test_search_spiderman2()
