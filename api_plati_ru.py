65d9cd2cda1b3327f56622bb97ae3c5b121ee63a
Friendweb <Friendrise@yandex.ru>
2024-04-14 23:43:27 +0300

    fix little bugs

--- api_plati_ru.py
+++ api_plati_ru.py
@@ -4,72 +4,87 @@ import tkinter as tk
 from tkinter import ttk
 from datetime import datetime
 import openpyxl
-import requests
+import aiohttp
+import asyncio
 from openpyxl.styles import PatternFill
-from concurrent.futures import ThreadPoolExecutor
+import json  # Добавляем импорт модуля json
 
-def fetch_data(query, page_size, page_end):
+async def fetch_data(session, query, page_size, page_end):
     url = f"https://plati.io/api/search.ashx?query={query}&pagesize={page_size}&pagenum={page_end}&visibleOnly=true&response=json"
-    response = requests.get(url)
-    data = response.json()
-    return data.get('items', [])
+    headers = {'Content-Type': 'application/json'}  # Указываем тип контента
+    async with session.get(url, headers=headers) as response:
+        data_text = await response.text()  # Читаем данные как текст
+        try:
+            data = json.loads(data_text)  # Преобразуем текст в JSON
+        except json.JSONDecodeError:
+            print("Ошибка при декодировании JSON")
+            data = {}
+        return data.get('items', [])
 
 def extract_account_info(description, title):
     keywords = ["аккаунт", "account", "аренда", "offline", "Оффлайн", "Онлайн", "п2", "п3", " п2 ", "п2-п3"]
     account_info = "Упоминание аккаунта" if any(keyword in title.lower() for keyword in keywords) else ""
     return account_info
 
-def add_data_to_excel(query, page_size=500, page_end=10, min_price=None, max_price=None):
+async def add_data_to_excel(session, params):
+    query = params['query']
+    page_size = params['page_size']
+    page_end = params['page_end']
+    min_price = params.get('min_price')
+    max_price = params.get('max_price')
+
     current_time = datetime.now()
     month_folder = current_time.strftime("%B")
     os.makedirs(month_folder, exist_ok=True)
     folder_path = os.path.abspath(month_folder)
     file_name = current_time.strftime(f"{month_folder}/%d_%m_%Y_%H_%M_{query}.xlsx")
-    wb = openpyxl.Workbook()
-    ws = wb.active
-    ws.append(["id", "Название", "Цена", "Ссылка", "Платформа", "Продавец", "Рейтинг продавца", "Положительные отзывы",
-               "Отрицательные отзывы", "Возвраты", "Упоминание аккаунта"])
 
-    all_items = fetch_all_items(query, page_size, page_end)
-    filtered_items = filter_items(all_items, min_price, max_price)
-    sorted_items = sort_items(filtered_items)
+    async with aiohttp.ClientSession() as session:
+        all_items = await fetch_all_items(session, query, page_size, page_end)
+        filtered_items = filter_items(all_items, min_price, max_price)
+        sorted_items = sorted(filtered_items, key=lambda x: x.get("price_rur", 0), reverse=True)
 
-    for idx, item in enumerate(sorted_items, start=1):
-        name = item.get("name", "")
-        price = item.get("price_rur", "")
-        url = item.get("url", "")
-        title = name.lower()
-        platform = extract_platform(title)
-        account_info = extract_account_info(item.get("description", ""), title)
-        ws.append(
-            [item.get("seller_id", ""), name, price, url, platform, item.get("seller_name", ""),
-             item.get("seller_rating", ""), item.get("count_positiveresponses", ""),
-             item.get("count_negativeresponses", ""), item.get("count_returns", ""), account_info])
+        wb = openpyxl.Workbook()
+        ws = wb.active
+        ws.append(["id", "Название", "Цена", "Ссылка", "Платформа", "Продавец", "Рейтинг продавца", "Положительные отзывы",
+                   "Отрицательные отзывы", "Возвраты", "Упоминание аккаунта"])
 
-        fill = None
-        seller_rating = item.get("seller_rating", "")
-        if seller_rating < 100:
-            fill = PatternFill(start_color="fdc0c8", end_color="fdc0c8", fill_type="solid")
-        elif 100 <= seller_rating < 500:
-            fill = PatternFill(start_color="fde890", end_color="fde890", fill_type="solid")
-        else:
-            fill = PatternFill(start_color="c0ecc7", end_color="c0ecc7", fill_type="solid")
+        for idx, item in enumerate(sorted_items, start=1):
+            name = item.get("name", "")
+            price = item.get("price_rur", "")
+            url = item.get("url", "")
+            title = name.lower()
+            platform = extract_platform(title)
+            account_info = extract_account_info(item.get("description", ""), title)
+            ws.append(
+                [item.get("seller_id", ""), name, price, url, platform, item.get("seller_name", ""),
+                 item.get("seller_rating", ""), item.get("count_positiveresponses", ""),
+                 item.get("count_negativeresponses", ""), item.get("count_returns", ""), account_info])
 
-        for cell in ws[idx + 1]:
-            cell.fill = fill
+            fill = None
+            seller_rating = item.get("seller_rating", "")
+            if seller_rating < 100:
+                fill = PatternFill(start_color="fdc0c8", end_color="fdc0c8", fill_type="solid")
+            elif 100 <= seller_rating < 500:
+                fill = PatternFill(start_color="fde890", end_color="fde890", fill_type="solid")
+            else:
+                fill = PatternFill(start_color="c0ecc7", end_color="c0ecc7", fill_type="solid")
 
-    for column_cells in ws.columns:
-        length = max(len(str(cell.value)) for cell in column_cells)
-        ws.column_dimensions[column_cells[0].column_letter].width = length + 2
+            for cell in ws[idx + 1]:
+                cell.fill = fill
 
-    ws.auto_filter.ref = ws.dimensions
-    wb.save(file_name)
+        for column_cells in ws.columns:
+            length = max(len(str(cell.value)) for cell in column_cells)
+            ws.column_dimensions[column_cells[0].column_letter].width = length + 2
+
+        ws.auto_filter.ref = ws.dimensions
+        wb.save(file_name)
+
     return folder_path
 
-def fetch_all_items(query, page_size, page_end):
-    with ThreadPoolExecutor() as executor:
-        futures = [executor.submit(fetch_data, query, page_size, page_num) for page_num in range(1, page_end + 1)]
-        return [item for future in futures for item in future.result()]
+async def fetch_all_items(session, query, page_size, page_end):
+    tasks = [fetch_data(session, query, page_size, page_num) for page_num in range(1, page_end + 1)]
+    return [item for sublist in await asyncio.gather(*tasks) for item in sublist]
 
 def filter_items(items, min_price, max_price):
     if min_price is not None:
@@ -80,9 +95,6 @@ def filter_items(items, min_price, max_price):
 
     return items
 
-def sort_items(items):
-    return sorted(items, key=lambda x: x.get("price_rur", 0), reverse=True)
-
 def extract_platform(title):
     platforms = ["Xbox", "PC", "PS", "Steam"]
     return next((platform for platform in platforms if platform.lower() in title), "")
@@ -91,13 +103,8 @@ def open_excel_folder():
     query = query_combobox.get()
     min_price = int(min_price_entry.get() or 0)
     max_price = int(max_price_entry.get() or 0)
-    month_folder = add_data_to_excel(query, min_price=min_price, max_price=max_price)
-    folder_path = os.path.abspath(month_folder)
-    if os.name == 'nt':
-        subprocess.Popen(f'explorer "{folder_path}"')
-    elif os.name == 'posix':
-        subprocess.Popen(['open', folder_path])
-    update_info(f"Данные сохранены в файле: {month_folder}")
+    params = {'query': query, 'page_size': 10, 'page_end': 10, 'min_price': min_price, 'max_price': max_price}
+    asyncio.run(add_data_to_excel(None, params))  # Здесь изменение
 
 def run_query():
     query = query_combobox.get()
@@ -113,7 +120,8 @@ def run_query():
 
     if query:
         info_text.delete(1.0, tk.END)
-        folder_path = add_data_to_excel(query, min_price=min_price, max_price=max_price)
+        params = {'query': query, 'page_size': 10, 'page_end': 10, 'min_price': min_price, 'max_price': max_price}
+        folder_path = asyncio.run(add_data_to_excel(None, params))  # Здесь изменение
         update_info(f"Данные сохранены в файле: {folder_path}")
     else:
         update_info("Введите запрос!")
